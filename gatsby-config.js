d6db4dc703b75d300d4f1bac8ce2f847bb3bab55
An Tran <tran.binhan@gmail.com>
2020-08-09 17:48:12 +0800

    Update configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,9 +37,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Gia Đình Tối Giản`,
+        short_name: `Gia Đình Tối Giản`,
+        description: `Blog của gia đình An, Liên, Manuel và Marie kể về lối sống tối giản.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
