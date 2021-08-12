37d22b35ff053d672e3fe451bef5c88fde1be964
antranapp <antranapp@users.noreply.github.com>
2021-08-12 21:53:14 +0000

    deployed via Gatsby Publish Action 🎩 for 3ac7b262a12ad6d88a28449d0bdd69b293833aca

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-a7cd51672cc83477391c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkthe_minimalist_family=self.webpackChunkthe_minimalist_family||[]).push([[306],{91930:function(e,t,n){var u=n(95318);t.__esModule=!0,t.default=void 0;var a=u(n(85354)),l=u(n(67294)),i=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return l.default.createElement(l.default.Fragment,null)},t}(l.default.Component);t.default=i}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-a7cd51672cc83477391c.js.map
